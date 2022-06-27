bc9efcc6bce369a78e6de20ce40655d9e6abc0c4
Eber Alejo <eber4202@hotmail.com>
2022-06-26 21:56:04 -0500

    add assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { ProductItem } from '../components/ProductItem'
 
 export const Home = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <ProductItem />
+    </>
+  )
 }
