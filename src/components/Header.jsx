2b1ebaba695f2306d9090c06f7458a7a2fde629e
Eber Alejo <eber4202@hotmail.com>
2022-06-29 21:26:49 -0500

    Feat manage products in menu and Add context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import '@styles/Header.scss'
 import { Menu } from '@components/Menu'
+import { MyOrder } from '@containers/MyOrder'
+import { AppContext } from '../context/AppContext'
 
 import menu from '@icons/icon_menu.svg'
 import logo from '@logos/logo_yard_sale.svg'
@@ -8,6 +10,8 @@ import shoppingCart from '@icons/icon_shopping_cart.svg'
 
 export const Header = () => {
   const [toggle, setToggle] = useState(false)
+  const [toggleOrders, setToggleOrders] = useState(false)
+  const { state } = useContext(AppContext)
 
   const handleToggle = () => {
     setToggle(!toggle)
@@ -46,13 +50,17 @@ export const Header = () => {
           <li className='navbar-email' onClick={handleToggle}>
             platzi@example.com
           </li>
-          <li className='navbar-shopping-cart'>
+          <li
+            className='navbar-shopping-cart'
+            onClick={() => setToggleOrders(!toggleOrders)}
+          >
             <img src={shoppingCart} alt='shopping cart' />
-            <div>2</div>
+            {state.cart.length > 0 && <div>{state.cart.length}</div>}
           </li>
         </ul>
       </div>
       {toggle && <Menu />}
+      {toggleOrders && <MyOrder />}
     </nav>
   )
 }
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,7 +5,7 @@ import { Home } from '../pages/Home'
 import { NotFound } from '../pages/NotFound'
 // containers
 import { Layout } from '../containers/Layout'
-import { Login } from '../containers/Login'
+import { Login } from '../pages/Login'
 import { RecoveryPassword } from '../containers/RecoveryPassword'
 import { SendEmail } from '../pages/SendEmail'
 import { NewPassword } from '../pages/NewPassword'
@@ -15,28 +15,35 @@ import { Checkout } from '../pages/Checkout'
 import { Orders } from '../pages/Orders'
 // styles
 import '../styles/global.scss'
+// context
+import { AppContext } from '../context/AppContext.js'
+// hooks
+import { useInitialState } from '@hooks/useInitialState'
 
 export const App = () => {
+  const initialState = useInitialState()
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/login' element={<Login />} />
-          <Route
-            exact
-            path='/recovery-password'
-            element={<RecoveryPassword />}
-          />
-          <Route exact path='/send-email' component={<SendEmail />} />
-          <Route exact path='/new-password' component={<NewPassword />} />
-          <Route exact path='/account' component={<MyAccount />} />
-          <Route exact path='/signup' component={<CreateAccount />} />
-          <Route exact path='/checkout' component={<Checkout />} />
-          <Route exact path='/orders' component={<Orders />} />
-          <Route exact path='*' element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/login' element={<Login />} />
+            <Route
+              exact
+              path='/recovery-password'
+              element={<RecoveryPassword />}
+            />
+            <Route exact path='/send-email' component={<SendEmail />} />
+            <Route exact path='/new-password' component={<NewPassword />} />
+            <Route exact path='/account' component={<MyAccount />} />
+            <Route exact path='/signup' component={<CreateAccount />} />
+            <Route exact path='/checkout' component={<Checkout />} />
+            <Route exact path='/orders' component={<Orders />} />
+            <Route exact path='*' element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   )
 }
